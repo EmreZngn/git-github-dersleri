c01951f41b00dcafd5ffaa15da6062cafc812404
EmreZengin <emrezengin@thy.com>
2024-11-22 09:38:28 +0300

    ilk sayinin büyük olması gerektiği kullanıcıya bildirildi

--- cikarma.js
+++ cikarma.js
@@ -1,3 +1,6 @@
 function cikar() {
-	sonuc.innerHTML = sayi1.value - sayi2.value;
+	if(parseInt(sayi1.value) < parseInt(sayi2.value))
+		alert("cikarma islemi için birinci sayinin ikinci sayidan büyük olması gerekiyor!")
+	else
+		sonuc.innerHTML = sayi1.value - sayi2.value;
 }
